e543da1ad63b8ad563b4b039ff62cc080b6b85ed
Replit user <>
2023-08-16 15:43:46 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -47,6 +47,7 @@ los números pares del arreglo original.
 int *filterEvenNumbers(int arr[], int size, int *newSize) {
   int j=0;
   int talla = *newSize;
+  int *pares = NULL;
   int *pares= malloc(talla*sizeof(int));
   for(int i=0; i<size; i++){
       if (arr[i]%2==0){
