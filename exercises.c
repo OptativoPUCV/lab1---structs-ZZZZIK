56ee6b9debff04cb2ea12345d9307be8d70bbb48
Replit user <>
2023-08-16 15:42:08 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -51,6 +51,7 @@ int *filterEvenNumbers(int arr[], int size, int *newSize) {
   for(int i=0; i<size; i++){
       if (arr[i]%2==0){
         pares[j]=arr[i];
+        j++;
       }
   }
   return pares; 
